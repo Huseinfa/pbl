052704c147b9b4dec6969ed2bc1fc99a24a191b9
= <=>
2025-11-26 15:57:30 +0700

    feat: form to db connection

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,11 +4,21 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\LetterController;
 
 Route::get("/user/{id}", [UserController::class, "show_user"]);
 Route::get('/test', function () {
     return response()->json(['status' => 'ok']);
 });
+
+
+
+Route::get('/letters', [LetterController::class, 'index']);
+Route::get('/letters/{id}', [LetterController::class, 'show']);
+Route::post('/letters', [LetterController::class, 'store']);
+Route::put('/letters/{id}/status', [LetterController::class, 'updateStatus']);
+Route::delete('/letters/{id}', [LetterController::class, 'destroy']);
+
 
 
 Route::post("/login", [AuthController::class, "login"]);
